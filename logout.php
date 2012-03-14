35fbb226eee32ae1a540f45e43ce7b627b21945a
Jeff <jgreen7779@gmail.com>
2012-03-14 17:01:15 -0400

    logout.php

---
+++ logout.php
@@ -0,0 +1,58 @@
+<html><head>
+    <title>SI Student Project Resource</title>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+    <div class="wrapper"><!--Wrapper for sticking footer to bottom of page-->
+	
+        <div id="headerContainer"><!--Header container-->
+            
+            <header id="header"><!--Header section-->
+				
+                <div id="siteName"><!--Site name and tagline-->
+                    <h1><a href="login.php">SI Student Project Resource</a></h1>
+                </div>
+            
+                <nav id="topNav"><!--Main site navigation-->
+                </nav>
+            
+            </header>
+            
+        </div><!--End header-->
+        
+<div id="guts"><!--Main content container-->
+            
+<div id="content"><!--Main content section-->
+	            
+	                
+<p><b>You have been logged out</b><p>
+    <br></br>
+    <p>Click here to <a href="login.php">Login!</a></p>
+
+</div>                           
+</div>
+<!--End content-->
+
+
+            
+</div><!--End guts-->
+        
+<div class="push"></div><!--Push for aligning footer at bottom of page-->
+	
+</div><!--End wrapper-->
+	
+<div id="footerContainer">
+	
+<div class="footer"><!--Site footer-->
+                        
+</div><!--End footer-->
+        
+</div><!--End footerContainer-->
+	
+
+
+</body></html>
+
