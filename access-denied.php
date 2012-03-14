279244ea01992b0b9ec28748eb249e8ff2ff4075
Jeff <jgreen7779@gmail.com>
2012-03-14 16:57:35 -0400

    acces-denied.php

---
+++ access-denied.php
@@ -0,0 +1,61 @@
+<html><head>
+    <title>SI Student Project Resource</title>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+    <div class="wrapper"><!--Wrapper for sticking footer to bottom of page-->
+	
+        <div id="headerContainer"><!--Header container-->
+            
+            <header id="header"><!--Header section-->
+				
+                <div id="siteName"><!--Site name and tagline-->
+                    <h1><a href="login.php">SI Student Project Resource</a></h1>
+                </div>
+            
+                <nav id="topNav"><!--Main site navigation-->
+                </nav>
+            
+            </header>
+            
+        </div><!--End header-->
+        
+<div id="guts"><!--Main content container-->
+            
+<div id="content"><!--Main content section-->
+	            
+	                
+<p><b>Access Denied!</b><p>
+<p>You must be a registered member to view this page</p>
+
+    <br></br>
+    <p>Already a member?  <a href="login.php">Login!</a></p>
+    
+    <p>Want to join?  <a href="register.php">Register!</a></p>
+    
+</div>                           
+</div>
+<!--End content-->
+
+
+            
+</div><!--End guts-->
+        
+<div class="push"></div><!--Push for aligning footer at bottom of page-->
+	
+</div><!--End wrapper-->
+	
+<div id="footerContainer">
+	
+<div class="footer"><!--Site footer-->
+                        
+</div><!--End footer-->
+        
+</div><!--End footerContainer-->
+	
+
+
+</body></html>
